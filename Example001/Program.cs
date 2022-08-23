722c0dace76b1737260a7ca18c9e802a0ed9ae22
NatalyaGorbonosova <gorbonosova.n@mail.ru>
2022-08-23 17:00:58 +0300

    Second Number

---
+++ Example001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру 
+//этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+int x = int.Parse(Console.ReadLine());
+
+int SecondNumber(int number)
+{
+    int x1 = number / 10;
+    int second = x1 % 10;
+    return second;
+}
+
+Console.WriteLine("Вторая цифра числа: " + SecondNumber(x));
