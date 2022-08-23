985bc5d227dd766dd1aec8273a8bbe5ef95951c9
NatalyaGorbonosova <gorbonosova.n@mail.ru>
2022-08-23 18:55:50 +0300

    Day Week

---
+++ Example003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число:");
+int x = int.Parse(Console.ReadLine());
+
+void DayWeek(int daynumber)
+{
+    if ((daynumber > 0) && (daynumber < 8)) 
+    {
+        if (daynumber < 6) Console.WriteLine("Нет, это рабочий день");
+        else Console.WriteLine("Да, это выходной!!!");
+    }
+    else Console.WriteLine("Введите цифру от 1 до 7");
+}
+
+DayWeek(x);
