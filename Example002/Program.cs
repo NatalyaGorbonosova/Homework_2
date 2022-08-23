876072bb01b58c0477c28e3ea9a5f491fc7e778d
NatalyaGorbonosova <gorbonosova.n@mail.ru>
2022-08-23 17:28:52 +0300

    Third Number

---
+++ Example002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+
+string ThirdNumber(int number)
+{
+    if (number >= 100)
+    {
+        int x1 = number / 100;
+        int result = x1 % 10;
+        return $"Третья цифра числаЖ: {result}";
+    }
+    else return "Третьей цифры нет";
+}
+
+Console.WriteLine(ThirdNumber(x));
